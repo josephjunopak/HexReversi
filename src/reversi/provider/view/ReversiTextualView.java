617d04006db38265ea425e765fc3ff3f9a8a0152
Vince <li.vin@northeastern.edu>
2023-12-05 15:16:33 -0500

    functioning game

---
+++ src/reversi/provider/view/ReversiTextualView.java
@@ -0,0 +1,13 @@
+package reversi.provider.view;
+
+/**
+ * Interface for the text view of a reversi game.
+ */
+public interface ReversiTextualView {
+
+  /**
+   * Renders a board.
+   * @return the formatted game
+   */
+  String toString();
+}
